6a61c4c65e3136155ed65c2f627439393869758c
chrpeters01 <chrisbpeters01@gmail.com>
2024-02-02 10:54:26 -0600

    Update main-view.jsx
    imageURL update

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,7 +7,7 @@ export const MainView = () => {
     { 
       id: 1, 
       title: "The Departed",
-      image: "https://media.themoviedb.org/t/p/original/nT97ifVT2J1yMQmeq20Qblg61T.jpg",
+      imageURL: "https://media.themoviedb.org/t/p/original/nT97ifVT2J1yMQmeq20Qblg61T.jpg",
       genre: "Crime",
       director: "Martin Scorsese",
       description: "South Boston cop Billy Costigan goes under cover to infiltrate the organization of gangland chief Frank Costello.",
@@ -16,7 +16,7 @@ export const MainView = () => {
     { 
       id: 2, 
       title: "Jurassic Park",
-      image: "https://media.themoviedb.org/t/p/original/oU7Oq2kFAAlGqbU4VoAE36g4hoI.jpg",
+      imageURL: "https://media.themoviedb.org/t/p/original/oU7Oq2kFAAlGqbU4VoAE36g4hoI.jpg",
       genre: "Action",
       director: "Steven Spielberg",
       description: "Paleontologists Alan Grant and Ellie Sattler and mathematician Ian Malcolm are among a select group chosen to tour an island theme park populated by dinosaurs created from prehistoric DNA.",
@@ -25,7 +25,7 @@ export const MainView = () => {
     { 
       id: 3, 
       title: "Avatar",
-      image: "https://media.themoviedb.org/t/p/original/kyeqWdyUXW608qlYkRqosgbbJyK.jpg",
+      imageURL: "https://media.themoviedb.org/t/p/original/kyeqWdyUXW608qlYkRqosgbbJyK.jpg",
       genre: "Animated",
       director: "James Cameron",
       description: "On the lush alien world of Pandora live the Na'vi, beings who appear primitive but are highly evolved. Because the planet's environment is poisonous, human/Na'vi hybrids, called Avatars, must link to human minds to allow for free movement on Pandora.",
