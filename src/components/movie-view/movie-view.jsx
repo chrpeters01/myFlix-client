056fdc51ca426de499c4dbca0f4332110f49e8ae
chrpeters01 <chrisbpeters01@gmail.com>
2024-03-05 12:47:58 -0600

    Update movie-view.jsx
    movie-view update

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -4,25 +4,31 @@ export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img id='movie-image' src={movie.ImageURL} alt='' />
-      </div>
-      <div>
-        <span>title: </span>
+        <span>Title: </span>
         <span>{movie.Title}</span>
       </div>
       <div>
-        <span>genre: </span>
+        <span>Genre: </span>
         <span>{movie.Genre.Name}</span>
       </div>
       <div>
-        <span>director: </span>
+        <span>Director: </span>
         <span>{movie.Director.Name}</span>
-      </div>
-     <div>
-        <span>description: </span>
-        <span>{movie.Description}</span>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
   );
+};
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+    })
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired
 };
