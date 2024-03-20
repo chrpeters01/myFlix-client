8e624ea110bebeac2eb6a060e1f687376b359beb
chrpeters01 <chrisbpeters01@gmail.com>
2024-03-20 10:33:34 -0500

    Update main-view.jsx
    revised to setUser, setToken

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -70,7 +70,11 @@ return (
                 <Navigate to="/" />
               ) : (
                 <Col md={5}>
-                  <LoginView onLoggedIn={(user) => setUser(user)} />
+                  <LoginView 
+                  onLoggedIn={(user, token) => {
+                    setUser(user);
+                    setToken(token);
+                  }} />
                 </Col>
               )}
             </>
